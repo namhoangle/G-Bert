88e60b26d706f16959f6136dc43aeffce2f0a566
Vũ Trọng Hiệu <v.hieuvt11@vinai.io>
2022-12-06 09:42:50 -0600

    train baseline

--- source/run_commands/train_baseline.sh
+++ source/run_commands/train_baseline.sh
@@ -1,2 +1,2 @@
 cd ../ &&
-python run_gbert.py --model_name GBert-baseline --use_pretrain --pretrain_dir ../saved/GBert-baseline --num_train_epochs 50 --do_train --graph
+python run_gbert.py --model_name GBert-baseline --use_pretrain --pretrain_dir ../saved/GBert-predict --num_train_epochs 50 --do_train --graph
